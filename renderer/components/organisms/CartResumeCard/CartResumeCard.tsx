bf257b53a078ba5f51fcab7b915632b64ca0f660
Alan Morales <alanmoraales845@gmail.com>
2021-06-23 01:15:46 -0500

    abstract resume variables

--- renderer/components/organisms/CartResumeCard/CartResumeCard.tsx
+++ renderer/components/organisms/CartResumeCard/CartResumeCard.tsx
@@ -5,6 +5,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
 import Grid from "components/atoms/Grid";
+import formatMoney from "utils/formatMoney";
 
 const Background = styled(Paper)({
   display: "grid",
@@ -12,26 +13,47 @@ const Background = styled(Paper)({
   padding: "16px",
 });
 
-const CartResumeCard = () => (
-  <Background variant="outlined">
-    <Typography variant="subtitle2">Resumen de la venta</Typography>
-    <Grid gap="16px">
-      <Typography variant="body2">Subtotal: $96.00</Typography>
-      <Typography variant="body2">Descuento: $12.00</Typography>
-      <Typography variant="subtitle2">Total: $84.00</Typography>
-    </Grid>
-    <Grid gap="16px">
-      <Typography variant="body2">Descuentos aplicados</Typography>
-      <Typography variant="subtitle2">Ruffles Queso 2x1</Typography>
-    </Grid>
-    <FormControlLabel
-      control={<Checkbox name="generateVoucher" color="primary" />}
-      label="¿Generar comprobante?"
-    />
-    <Button variant="contained" color="primary">
-      Terminar
-    </Button>
-  </Background>
-);
+// TODO bring this data from cart context
+const subtotal = 96;
+const discount = 12;
+const total = 84;
+const appliedDiscounts = [
+  {
+    id: "exampleid1",
+    description: "Ruffles Queso 2x1",
+  },
+];
+
+const CartResumeCard = () => {
+  const formatedSubtotal = formatMoney(subtotal);
+  const formatedDiscount = formatMoney(discount);
+  const formatedTotal = formatMoney(total);
+
+  return (
+    <Background variant="outlined">
+      <Typography variant="subtitle2">Resumen de la venta</Typography>
+      <Grid gap="16px">
+        <Typography variant="body2">Subtotal: {formatedSubtotal}</Typography>
+        <Typography variant="body2">Descuento: {formatedDiscount}</Typography>
+        <Typography variant="subtitle2">Total: {formatedTotal}</Typography>
+      </Grid>
+      <Grid gap="16px">
+        <Typography variant="body2">Descuentos aplicados</Typography>
+        {appliedDiscounts.map(({ id, description }) => (
+          <Typography variant="subtitle2" key={id}>
+            {description}
+          </Typography>
+        ))}
+      </Grid>
+      <FormControlLabel
+        control={<Checkbox name="generateVoucher" color="primary" />}
+        label="¿Generar comprobante?"
+      />
+      <Button variant="contained" color="primary">
+        Terminar
+      </Button>
+    </Background>
+  );
+};
 
 export default CartResumeCard;
