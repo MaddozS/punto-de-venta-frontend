0419a67e5ec0f7772d18415d83fcab7a406ee7bc
Alan Morales <alanmoraales845@gmail.com>
2021-06-22 21:33:27 -0500

    add Sales section atoms

---
+++ renderer/components/atoms/Sales/HeaderContainer.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+
+const HeaderContainer = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+export { HeaderContainer };
--- renderer/components/templates/SectionLayout/SectionLayout.tsx
+++ renderer/components/templates/SectionLayout/SectionLayout.tsx
@@ -1,24 +1,12 @@
-import {FC} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { FC } from "react";
+import { styled } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-const useSectionLayoutStyles = makeStyles({
-  container: {
-    display: 'grid',
-    gap: '64px',
-    paddingBottom: '64px',
-    paddingTop: '64px',
-  },
+const SectionLayout = styled(Container)({
+  display: "grid",
+  gap: "64px",
+  paddingBottom: "64px",
+  paddingTop: "64px",
 });
 
-const SectionLayout: FC = ({children}) => {
-  const styles = useSectionLayoutStyles()
-
-  return (
-    <Container className={styles.container}>
-      {children}
-    </Container>
-  )
-}
-
-export default SectionLayout
+export default SectionLayout;
