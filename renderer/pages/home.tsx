f031ff377549a19959dc44e88c935b21c006d767
Alan Morales <alanmoraales845@gmail.com>
2021-06-22 21:51:07 -0500

    add sales section

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import MainLayout from '../components/templates/MainLayout';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import SectionLayout from "../components/templates/SectionLayout";
+import Flex from "../components/atoms/Flex";
+import { HeaderContainer } from "../components/atoms/Sales";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      textAlign: 'center',
-      paddingTop: theme.spacing(4),
-    },
-  })
+const Sales = () => (
+  <SectionLayout>
+    <HeaderContainer>
+      <Typography variant="h5">Ventas</Typography>
+      <Flex>
+        <Button variant="outlined">Generar Utilidad</Button>
+        <Flex gap="8px" alignItems="center">
+          <Typography variant="body1">Vendido hoy:</Typography>
+          <Typography variant="h5">$717.50</Typography>
+        </Flex>
+      </Flex>
+    </HeaderContainer>
+    <Flex gap="32px" direction="column">
+      <Flex>
+        <Typography variant="subtitle2">Nueva Venta</Typography>
+      </Flex>
+    </Flex>
+  </SectionLayout>
 );
 
-function Home() {
-  const classes = useStyles({});
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => setOpen(false);
-  const handleClick = () => setOpen(true);
-
-  return (
-    <MainLayout></MainLayout>
-  );
-};
-
-export default Home;
+export default Sales;
