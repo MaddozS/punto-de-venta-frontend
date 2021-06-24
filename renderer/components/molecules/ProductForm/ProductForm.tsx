9cf09fef9753de03de4f2c28255e3e36c2d80be3
Alan Morales <alanmoraales845@gmail.com>
2021-06-24 03:14:10 -0500

    add product form validation

--- renderer/components/molecules/ProductForm/ProductForm.tsx
+++ renderer/components/molecules/ProductForm/ProductForm.tsx
@@ -1,50 +1,103 @@
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-material-ui";
 import Flex from "components/atoms/Flex";
 
-// TODO add logic
-const ProductForm = () => (
-  <Flex
-    direction="column"
-    justifyContent="space-between"
-    height="100%"
-    gap="32px"
-  >
-    <Flex gap="32px" direction="column">
-      <Typography variant="h5">Nuevo Producto</Typography>
-      <Flex gap="16px" direction="column">
-        <TextField variant="outlined" label="Nombre" name="name" />
-        <TextField
-          id="outlined-adornment-amount"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-          }}
-          variant="outlined"
-          label="Precio"
-          type="tel"
-          name="price"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          variant="outlined"
-          label="Descripción"
-          name="description"
-          multiline
-          rows={6}
-        />
-      </Flex>
-    </Flex>
-    <Flex justifyContent="flex-end" gap="16px">
-      <Button variant="outlined">Cancelar</Button>
-      <Button color="primary" variant="contained">
-        Agregar Producto
-      </Button>
-    </Flex>
-  </Flex>
-);
+interface IProductFormValues {
+  name: string;
+  price: number;
+  description?: string;
+}
+
+interface IValidateFormValues {
+  name: string;
+  price: string;
+  description?: string;
+}
+
+const ProductForm = () => {
+  const onSubmit = (values: IProductFormValues) => {
+    console.log({ values });
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        description: "",
+        price: undefined,
+      }}
+      validate={(values) => {
+        const errors: Partial<IValidateFormValues> = {};
+        if (!values.name) {
+          errors.name = "El nombre es requerido";
+        }
+        if (!values.price) {
+          errors.price = "El número es requerido";
+        } else if (isNaN(values.price)) {
+          errors.price = "Introduce un número válido";
+        }
+        return errors;
+      }}
+      onSubmit={onSubmit}
+    >
+      {({ submitForm, errors }) => (
+        <Form>
+          <Flex
+            direction="column"
+            justifyContent="space-between"
+            height="100%"
+            gap="32px"
+          >
+            <Flex gap="32px" direction="column">
+              <Typography variant="h5">Nuevo Producto</Typography>
+              <Flex gap="16px" direction="column">
+                <Field
+                  label="Nombre *"
+                  name="name"
+                  variant="outlined"
+                  component={TextField}
+                />
+                <Field
+                  label="Precio *"
+                  name="price"
+                  variant="outlined"
+                  component={TextField}
+                  type="number"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">$</InputAdornment>
+                    ),
+                  }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <Field
+                  label="Descripción"
+                  name="description"
+                  multiline
+                  rows={6}
+                  variant="outlined"
+                  component={TextField}
+                />
+              </Flex>
+            </Flex>
+            <Flex justifyContent="flex-end" gap="16px">
+              <Button variant="outlined" type="button">
+                Cancelar
+              </Button>
+              <Button color="primary" variant="contained" type="submit">
+                Agregar Producto
+              </Button>
+            </Flex>
+          </Flex>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 export default ProductForm;
