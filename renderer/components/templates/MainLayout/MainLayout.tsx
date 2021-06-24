94ca19f114410d17b5369c72843b0a7d160e6d43
Alan Morales <alanmoraales845@gmail.com>
2021-06-23 23:18:19 -0500

    add sidebar

--- renderer/components/templates/MainLayout/MainLayout.tsx
+++ renderer/components/templates/MainLayout/MainLayout.tsx
@@ -1,5 +1,61 @@
-const MainLayout = () => (
-    <div>Tienda</div>
-)
+import { FC } from "react";
+import { styled } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "components/atoms/Grid";
+import Flex from "components/atoms/Flex";
+import Link from "components/atoms/Link";
 
-export default MainLayout
+// padding="64px 32px"
+const Container = styled(Grid)({
+  height: "100vh",
+  gridTemplateColumns: "auto 1fr",
+  overflow: "hidden",
+});
+
+const ContentContainer = styled(Box)({
+  height: "100%",
+  overflowY: "scroll",
+});
+
+const ItemText = styled(ListItemText)({
+  paddingLeft: "16px",
+});
+
+const menuItems = [
+  {
+    label: "Ventas",
+    href: "/home",
+  },
+  {
+    label: "Productos",
+    href: "/products",
+  },
+];
+
+const MainLayout: FC = ({ children }) => (
+  <Container>
+    <Flex direction="column" borderRight="thin gray solid">
+      <Box padding="64px 32px">
+        <Typography variant="h5">Tiendita</Typography>
+      </Box>
+      <List>
+        {menuItems.map(({ label, href }, index) => (
+          <Link key={label} href={href} underline="none" color="textPrimary">
+            <ListItem button>
+              <ItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </Flex>
+    <ContentContainer>{children}</ContentContainer>
+  </Container>
+);
+
+export default MainLayout;
--- renderer/pages/products.tsx
+++ renderer/pages/products.tsx
@@ -1,6 +1,7 @@
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import SectionLayout from "components/templates/SectionLayout";
+import MainLayout from "components/templates/MainLayout";
 import ProductsTable from "components/organisms/ProductsTable";
 import NewProductForm from "components/organisms/NewProductForm";
 import Modal from "components/molecules/Modal";
@@ -14,22 +15,24 @@ const Products = () => {
 
   return (
     <>
-      <SectionLayout>
-        <Typography variant="h5">Productos</Typography>
-        <Grid gap="32px">
-          <Flex justifyContent="space-between" alignItems="center">
-            <Typography variant="subtitle2">Lista de productos</Typography>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={toggleNewProductForm}
-            >
-              Nuevo Producto
-            </Button>
-          </Flex>
-          <ProductsTable />
-        </Grid>
-      </SectionLayout>
+      <MainLayout>
+        <SectionLayout>
+          <Typography variant="h5">Productos</Typography>
+          <Grid gap="32px">
+            <Flex justifyContent="space-between" alignItems="center">
+              <Typography variant="subtitle2">Lista de productos</Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={toggleNewProductForm}
+              >
+                Nuevo Producto
+              </Button>
+            </Flex>
+            <ProductsTable />
+          </Grid>
+        </SectionLayout>
+      </MainLayout>
       <Modal
         open={newProductFormIsOpen}
         onClose={toggleNewProductForm}
