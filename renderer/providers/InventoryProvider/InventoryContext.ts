723dac390e6e0a76f2d62ed9f53ee953ba13c757
Alan Morales <alanmoraales845@gmail.com>
2021-06-29 23:10:44 -0500

    add products to inventory provider

--- renderer/providers/InventoryProvider/InventoryContext.ts
+++ renderer/providers/InventoryProvider/InventoryContext.ts
@@ -1,10 +1,12 @@
 import { createContext } from "react";
 import type { IInventory } from "shared/types/inventory";
+import type { IProduct } from "shared/types/product";
 
 interface IInventoryContext {
   inventory: IInventory[];
   loadingInventory: boolean;
   errorLoadingInventory: boolean;
+  products: IProduct[];
 }
 
 const InventoryContext = createContext<IInventoryContext | undefined>(
--- renderer/providers/InventoryProvider/InventoryProvider.tsx
+++ renderer/providers/InventoryProvider/InventoryProvider.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 import { InventoryContext, IInventoryContext } from "./InventoryContext";
 import { useInventory } from "hooks/inventory";
+import { useProducts } from "hooks/products";
 
 const InventoryProvider: FC = ({ children }) => {
   const { inventory, loadingInventory, errorLoadingInventory } = useInventory();
+  const { products } = useProducts();
 
   const contextValue: IInventoryContext = {
     inventory,
     loadingInventory,
     errorLoadingInventory,
+    products,
   };
 
   return (
